c67d4b21ca0a8ec8a04cf1f34784603be3caaece
ayyleesh <xxserpentxbizzarexx@yahoo.co.id>
2019-04-26 01:16:06 +0100

    added enemy spawn functionality

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnDelay;
+
+    public Transform spawnPoint;
+
+     void Start()
+    {
+        spawnPoint = GetComponent<Transform>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Invoke("Spawn", spawnDelay);
+        }
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+    }
+}
